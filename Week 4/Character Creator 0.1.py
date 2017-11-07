f1f86803255aa35af100bc8685ae66ac00341ea1
Xepholas <rpott@hotmail.co.uk>
2017-11-07 11:32:03 +0000

    Delete Character Creator 0.1.py

--- Week 4/Character Creator 0.1.py
+++
@@ -1,6 +0,0 @@
-#Character Creator 0.1
-import random
-import time
-
-print("Welcome to the character creation menu!")
-print("\nHere, you can build a character of your choice by using attribute points.")
